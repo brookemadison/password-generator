83eb50ec21207d2d654e98c0e7a7960571097edf
brookemadison <brookeperry611@gmail.com>
2021-07-09 23:01:07 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Assignment code here
+function generatePassword() {
+  var length = 16,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$'()*+,-./:;<=>?@[_`{|}~",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function passwordCriteria(){
+  var userAdjective = prompt("Please provide an Adjective");
+  alert (userAdjective);
+}
